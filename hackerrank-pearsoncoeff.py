368ed512ec6a3ebed48382a247b1c52de2d7f2a4
Bikramjeet Singh Bedi <96697911+bkrmm@users.noreply.github.com>
2025-07-03 03:44:15 +0530

    Update hackerrank-pearsoncoeff.py

--- hackerrank-pearsoncoeff.py
+++ hackerrank-pearsoncoeff.py
@@ -1,9 +1,15 @@
+physics = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
+history = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
 
-from scipy.stats import pearsonr
+n = len(physics)
 
-physics_scores = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
-history_scores = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
+mean_x = sum(physics) / n
+mean_y = sum(history) / n
 
-r = pearsonr(physics_scores, history_scores)
+sum_xy = sum((physics[i] - mean_x) * (history[i] - mean_y) for i in range(n))
+sum_xx = sum((physics[i] - mean_x)**2 for i in range(n))
+sum_yy = sum((history[i] - mean_y)**2 for i in range(n))
 
-print(f"{r[0]:.3f}")
+r = sum_xy / (sum_xx*sum_yy)**(1/2)
+
+print(f"{r:.3f}")
